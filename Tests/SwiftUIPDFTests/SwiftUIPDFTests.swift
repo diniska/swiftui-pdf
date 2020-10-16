ad61d46a6943dc94ba9a5e6cd51d0f6c2fb4b232
Denis Chaschin <denis@chaschin.pro>
2020-10-17 10:54:54 +1100

    Test placeholder removed

--- Tests/SwiftUIPDFTests/SwiftUIPDFTests.swift
+++ Tests/SwiftUIPDFTests/SwiftUIPDFTests.swift
@@ -2,14 +2,6 @@ import XCTest
 @testable import SwiftUIPDF
 
 final class SwiftUIPDFTests: XCTestCase {
-    func testExample() {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct
-        // results.
-        XCTAssertEqual(SwiftUIPDF().text, "Hello, World!")
-    }
-
-    static var allTests = [
-        ("testExample", testExample),
+    static var allTests: [(String, () -> ())] = [
     ]
 }
