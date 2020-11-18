a359991d92bf98017385ffc7b0a6cc12a6f268e5
Denis Chaschin <denis@chaschin.pro>
2020-11-18 19:10:09 +1100

    PDFPageView implemented for MacOS

--- Sources/SwiftUIPDF/PDFPageView+AppKit.swift
+++ Sources/SwiftUIPDF/PDFPageView+AppKit.swift
@@ -1,8 +1,39 @@
 //
-//  File.swift
+//  PDFPageView+AppKit.swift
 //  
 //
 //  Created by Denis Chaschin on 18.11.2020.
 //
 
-import Foundation
+#if canImport(PDFKit) && canImport(AppKit) && canImport(SwiftUI) && canImport(Combine)
+
+import AppKit
+import SwiftUI
+import PDFKit
+
+/// A view that displays a single PDF page
+@available(macOS 10.15, *)
+extension PDFPageView: NSViewRepresentable {
+    public typealias NSViewType = AutoresizablePDFPageView
+    
+    public func makeNSView(context: Context) -> NSViewType {
+        let view = NSViewType(frame: .zero)
+        view.layer?.backgroundColor = .clear
+        view.setContentCompressionResistancePriority(.defaultLow, for: .horizontal)
+        view.setContentCompressionResistancePriority(.defaultLow, for: .vertical)
+        view.setContentHuggingPriority(.defaultLow, for: .horizontal)
+        view.setContentHuggingPriority(.defaultLow, for: .vertical)
+        return view
+    }
+    
+    public func updateNSView(_ pdfPageView: NSViewType, context: Context) {
+        pdfPageView.page = page
+    }
+    
+    public static func dismantleNSView(_ pdfPageView: NSViewType, coordinator: ()) {
+        pdfPageView.page = nil
+    }
+    
+}
+
+#endif
